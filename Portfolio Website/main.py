a3b51cb87d48b36f27855c4ff3114af67130e23e
Erchey <okpalakingsleyeche@gmail.com>
2024-03-04 04:44:30 +0100

    new file:   Portfolio Website/index.html 	new file:   Portfolio Website/main.py 	new file:   Portfolio Website/static/css/style.css

---
+++ Portfolio Website/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import requests
+
+# posts = requests.get("https://api.npoint.io/4fd68aac02b8d7330eac").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+app.run(debug=True)
